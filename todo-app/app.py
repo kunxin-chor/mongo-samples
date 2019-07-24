84c3912e21b358d2ac57962a81b0f38b8d0c31e5
Ubuntu <ubuntu@ip-172-31-30-25.ap-southeast-1.compute.internal>
2019-07-24 02:59:57 +0000

    Show a basic todo-list

--- todo-app/app.py
+++ todo-app/app.py
@@ -1,16 +1,30 @@
 from flask import Flask, render_template, request, redirect
 import os
+import pymongo
 
 app = Flask(__name__)
+
+# STEP 0 - Create the connection to mongo
+# 0.1. Retrieve the environment variables
+MONGO_URI = os.getenv('MONGO_URI')
+DATABASE_NAME = 'todos'
+TASKS = 'tasks'
+
+# 0.2. Create the connection
+conn = pymongo.MongoClient(MONGO_URI)
+
+
 
 # STEP 1 - Create a home route and test it
 @app.route('/') # map the root route to the index function
 def index():
     
-    # STEP 2 - Blank, no logic
+    # STEP 2 - Fetch all the existing todos --> as a Python dictionary
+    results = conn[DATABASE_NAME][TASKS].find({})
     
-    # STEP 3 - Return a template
-    return render_template('index.html')
+    
+    # STEP 3 - Return a template and assign the results to a placeholder in the template
+    return render_template('index.html', data=results)
 
 
 # "magic code" -- boilerplate
